ee7a2f6404c7667c6c82b36a4e3e8be72c459a40
Bhavik <bhavik.m@communicationcrafts.com>
2024-09-05 16:25:53 +0530

    add new count

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
       <h1>Vite + React  </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count  + 1)}>
-          count is {count}
+          count is 67888
         </button>
         <p>
         Edit <code>src/App.jsx</code> 
