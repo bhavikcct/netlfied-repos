c08d82dc1db85629dc2c387c2c344fd7402942d6
Bhavik <bhavik.m@communicationcrafts.com>
2024-09-05 15:33:11 +0530

    comment service workers

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-      Click on the Vite and React logos to learn more czdvsdxvsdvsdcfsfwedw
+      Click on the Vite and React logos to learn more 
 
       </p>
     </>
