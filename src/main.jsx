db8275b7543c91e8633f77e515c75b4f3fa885b2
Bhavik <bhavik.m@communicationcrafts.com>
2024-09-05 15:31:07 +0530

    remove worker thread

--- src/main.jsx
+++ src/main.jsx
@@ -11,39 +11,39 @@ createRoot(document.getElementById("root")).render(
     </StrictMode>
   </BrowserRouter>
 );
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((registration) => {
-        console.log(
-          "ServiceWorker registration successful with scope: ",
-          registration.scope
-        );
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", () => {
+//     navigator.serviceWorker
+//       .register("/service-worker.js")
+//       .then((registration) => {
+//         console.log(
+//           "ServiceWorker registration successful with scope: ",
+//           registration.scope
+//         );
 
-        registration.onupdatefound = () => {
-          const installingWorker = registration.installing;
-          installingWorker.onstatechange = () => {
-            if (installingWorker.state === "installed") {
-              if (navigator.serviceWorker.controller) {
-                // New content is available; prompt the user to refresh
-                console.log("New content is available; please refresh.");
-                if (
-                  window.confirm(
-                    "New version available! Would you like to update?"
-                  )
-                ) {
-                  window.location.reload();
-                }
-              } else {
-                console.log("Content cached for offline use.");
-              }
-            }
-          };
-        };
-      })
-      .catch((error) => {
-        console.error("ServiceWorker registration failed: ", error);
-      });
-  });
-}
+//         registration.onupdatefound = () => {
+//           const installingWorker = registration.installing;
+//           installingWorker.onstatechange = () => {
+//             if (installingWorker.state === "installed") {
+//               if (navigator.serviceWorker.controller) {
+//                 // New content is available; prompt the user to refresh
+//                 console.log("New content is available; please refresh.");
+//                 if (
+//                   window.confirm(
+//                     "New version available! Would you like to update?"
+//                   )
+//                 ) {
+//                   window.location.reload();
+//                 }
+//               } else {
+//                 console.log("Content cached for offline use.");
+//               }
+//             }
+//           };
+//         };
+//       })
+//       .catch((error) => {
+//         console.error("ServiceWorker registration failed: ", error);
+//       });
+//   });
+// }
